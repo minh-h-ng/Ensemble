6e3bd297ca4228e4ff7530196f87c47cd3e8efd8
Gaurav Kalra <gvkalra@gmail.com>
2017-11-09 20:29:32 +0900

    reducing file io
    read csv only once

--- timeseries/evolve.py
+++ timeseries/evolve.py
@@ -27,174 +27,197 @@ creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 
-# Initial population is seeded from json file
-def initPopulation(pcls, ind_init, filename):
-    with open(filename, "r") as pop_file:
-        contents = json.load(pop_file)
-    return pcls(ind_init(c) for c in contents)
+class GeneticAlgorithm:
+    def __init__(self, file_path, samples=500,
+                 cxpb=0.90, mutpb=0.10, ngen=100,
+                 selbest=50):
+        """
+        Initializes data required by genetic algorithm
+        :param file_path: path to output of forecast.py
+        :param samples: clip algorithm to most recent 500 forecasts
+        :param cxpb: probability of mating two individuals
+        :param mutpb: probability of mutating an individual
+        :param ngen: number of generations
+        :param selbest: number of best individuals to select
+        """
+        # Read data frame
+        self.df = pd.read_csv(
+            file_path,
+            header=0,
+            usecols=['Naive', 'AR', 'ARMA', 'ARIMA',
+                     'ETS', 'CurrentObservation']
+        )
 
+        # Clip
+        if samples <= 0:
+            raise ValueError
+        self.clip = samples
 
-def computeElasticityIndex(row):
-    return min(row['GA'], row['CurrentObservation']) / max(row['GA'], row['CurrentObservation'])
+        # Parameters
+        self.cxpb = cxpb
+        self.mutpb = mutpb
+        self.ngen = ngen
+        self.selbest = selbest
 
+    # Initial population is seeded from json file
+    def initPopulation(self, pcls, ind_init, filename):
+        with open(filename, "r") as pop_file:
+            contents = json.load(pop_file)
+        return pcls(ind_init(c) for c in contents)
 
-def computeFitness(individual, hours_elapsed):
-    # script's directory
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(dir_path, '..', 'PythonESN', 'data_backup', 'edgar')
+    def computeElasticityIndex(self, row):
+        return min(row['GA'], row['CurrentObservation']) / max(row['GA'], row['CurrentObservation'])
 
-    # WARN: the data must be sampled at every hour for nrows to work!
+    def computeFitness(self, individual, hours_elapsed):
+        # df subset
+        if hours_elapsed >= self.clip:
+            start_idx = hours_elapsed - (self.clip - 1)
+            end_idx = self.clip + 1
+            dfs = self.df[start_idx:end_idx].copy()
+        else:
+            dfs = self.df[:hours_elapsed].copy()
 
-    # read frame
-    df = pd.read_csv(file_path, nrows=hours_elapsed)
+        # compute predicted value in hours_elapsed
+        dfs['GA'] = dfs[['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS']].dot(individual).round()
 
-    # compute predicted value in hours_elapsed
-    if hours_elapsed >= 500:
-        start_idx = hours_elapsed - (500 - 1)
-        df['GA'] = df[['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS']][start_idx:(500 + 1)] \
-            .dot(individual).round()
-    else:
-        df['GA'] = df[['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS']].dot(individual).round()
+        # compute SEI for predictions in hours_elapsed
+        return dfs.apply(self.computeElasticityIndex, axis=1).sum(),
 
-    # compute SEI for predictions in hours_elapsed
-    return df.apply(computeElasticityIndex, axis=1).sum(),
+    def cxArithmeticAverage(self, ind1, ind2):
+        _os = [sum(x) / 2 for x in zip(ind1, ind2)]
+        return creator.Individual(_os)
 
+    def mutSwapGenes(self, individual):
+        size = len(individual)
 
-def cxArithmeticAverage(ind1, ind2):
-    _os = [sum(x) / 2 for x in zip(ind1, ind2)]
-    return creator.Individual(_os)
+        rand = np.random.choice(range(size), 2, replace=False)
+        indx1 = rand[0]
+        indx2 = rand[1]
 
+        individual[indx1], individual[indx2] = \
+            individual[indx2], individual[indx1]
 
-def mutSwapGenes(individual):
-    size = len(individual)
+        return individual,
 
-    rand = np.random.choice(range(size), 2, replace=False)
-    indx1 = rand[0]
-    indx2 = rand[1]
+    def varTwoByTwo(self, population, toolbox, cxpb, mutpb):
+        offspring = []
 
-    individual[indx1], individual[indx2] = \
-        individual[indx2], individual[indx1]
+        # "two-by-two" combinations
+        for ind1_idx in range(len(population)):
+            for ind2_idx in range(ind1_idx + 1, len(population)):
+                if random.random() < cxpb:
+                    # clone
+                    ind1 = toolbox.clone(population[ind1_idx])
+                    ind2 = toolbox.clone(population[ind2_idx])
 
-    return individual,
+                    # mate
+                    _os = toolbox.mate(ind1, ind2)
+                    del _os.fitness.values
+                    offspring.append(_os)
 
+        # mutation
+        for i in range(len(offspring)):
+            if random.random() < mutpb:
+                offspring[i], = toolbox.mutate(offspring[i])
+                del offspring[i].fitness.values
 
-def init_toolbox():
-    toolbox = base.Toolbox()
+        return offspring
 
-    # Population is a list of individuals seeded from json file
-    toolbox.register("population", initPopulation, list, creator.Individual, "seed_population.json")
-
-    # Evaluation: elasticity index
-    toolbox.register("evaluate", computeFitness)
-
-    # Selection: best 50 individuals
-    toolbox.register("select", tools.selBest, k=50)
-
-    # Crossover: Arithmetic average
-    toolbox.register("mate", cxArithmeticAverage)
-
-    # Mutation: swap genes
-    toolbox.register("mutate", mutSwapGenes)
-
-    # multiprocessing
-    toolbox.register("map", futures.map)
-
-    return toolbox
-
-
-def varTwoByTwo(population, toolbox, cxpb, mutpb):
-    offspring = []
-
-    # "two-by-two" combinations
-    for ind1_idx in range(len(population)):
-        for ind2_idx in range(ind1_idx + 1, len(population)):
-            if random.random() < cxpb:
-                # clone
-                ind1 = toolbox.clone(population[ind1_idx])
-                ind2 = toolbox.clone(population[ind2_idx])
-
-                # mate
-                _os = toolbox.mate(ind1, ind2)
-                del _os.fitness.values
-                offspring.append(_os)
-
-    # mutation
-    for i in range(len(offspring)):
-        if random.random() < mutpb:
-            offspring[i], = toolbox.mutate(offspring[i])
-            del offspring[i].fitness.values
-
-    return offspring
-
-
-def eaValter(population, toolbox, cxpb, mutpb, ngen, hours_elapsed,
-             stats=None, halloffame=None, verbose=__debug__):
-    logbook = tools.Logbook()
-    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
-
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in population if not ind.fitness.valid]
-    fitnesses = toolbox.map(partial(toolbox.evaluate, hours_elapsed=hours_elapsed), invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
-        ind.fitness.values = fit
-
-    if halloffame is not None:
-        halloffame.update(population)
-
-    # save the current best individual
-    best_ind = toolbox.clone(halloffame[-1])
-
-    record = stats.compile(population) if stats is not None else {}
-    logbook.record(gen=0, nevals=len(invalid_ind), **record)
-    if verbose:
-        logger.debug(logbook.stream)
-
-    # Begin the generational process
-    for gen in range(1, ngen + 1):
-        # Vary the population
-        offspring = varTwoByTwo(population, toolbox, cxpb, mutpb)
+    def eaValter(self, population, toolbox, cxpb, mutpb, ngen, hours_elapsed,
+                 stats=None, halloffame=None, verbose=__debug__):
+        logbook = tools.Logbook()
+        logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
 
         # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        invalid_ind = [ind for ind in population if not ind.fitness.valid]
         fitnesses = toolbox.map(partial(toolbox.evaluate, hours_elapsed=hours_elapsed), invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
 
-        # Update the hall of fame with the generated individuals
         if halloffame is not None:
-            halloffame.update(offspring)
+            halloffame.update(population)
 
-        # break if best_ind is not changed
-        if best_ind == halloffame[-1]:
-            break
+        # save the current best individual
+        best_ind = toolbox.clone(halloffame[-1])
 
-        # Select the next generation population
-        population[:] = toolbox.select(population + offspring)
-
-        # Update the statistics with the new population
         record = stats.compile(population) if stats is not None else {}
-        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        logbook.record(gen=0, nevals=len(invalid_ind), **record)
         if verbose:
             logger.debug(logbook.stream)
 
-    return population, logbook
+        # Begin the generational process
+        for gen in range(1, ngen + 1):
+            # Vary the population
+            offspring = self.varTwoByTwo(population, toolbox, cxpb, mutpb)
 
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = toolbox.map(partial(toolbox.evaluate, hours_elapsed=hours_elapsed), invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
 
-def run_ga(hours_elapsed):
-    toolbox = init_toolbox()
+            # Update the hall of fame with the generated individuals
+            if halloffame is not None:
+                halloffame.update(offspring)
 
-    population = toolbox.population()
+            # break if best_ind is not changed
+            if best_ind == halloffame[-1]:
+                break
 
-    # Parameters
-    cxpb = 0.90  # Probability of mating two individuals
-    mutpb = 0.10  # Probability of mutating an individual
-    ngen = 100  # Number of generations
+            # Select the next generation population
+            population[:] = toolbox.select(population + offspring)
 
-    # Best individual
-    halloffame = tools.HallOfFame(maxsize=1)
-    final_population, logbook = eaValter(population, toolbox, cxpb, mutpb, ngen,
-                                         hours_elapsed=hours_elapsed, halloffame=halloffame)
-    return halloffame[-1]
+            # Update the statistics with the new population
+            record = stats.compile(population) if stats is not None else {}
+            logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+            if verbose:
+                logger.debug(logbook.stream)
+
+        return population, logbook
+
+    def init_toolbox(self):
+        toolbox = base.Toolbox()
+
+        # Population is a list of individuals seeded from json file
+        toolbox.register("population", self.initPopulation,
+                         list, creator.Individual, "seed_population.json")
+
+        # Evaluation: elasticity index
+        toolbox.register("evaluate", self.computeFitness)
+
+        # Selection: best 50 individuals
+        toolbox.register("select", tools.selBest, k=self.selbest)
+
+        # Crossover: Arithmetic average
+        toolbox.register("mate", self.cxArithmeticAverage)
+
+        # Mutation: swap genes
+        toolbox.register("mutate", self.mutSwapGenes)
+
+        # multiprocessing
+        toolbox.register("map", futures.map)
+
+        return toolbox
+
+    def run(self, hours_elapsed):
+        toolbox = self.init_toolbox()
+
+        population = toolbox.population()
+
+        # Best individual
+        halloffame = tools.HallOfFame(maxsize=1)
+        final_population, logbook = self.eaValter(population, toolbox,
+                                                  self.cxpb, self.mutpb, self.ngen,
+                                                  hours_elapsed=hours_elapsed,
+                                                  halloffame=halloffame)
+
+        # read genes at hours_elapsed
+        # iloc is zero-indexed
+        genes = self.df.iloc[hours_elapsed - 1][['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS']]
+
+        # compute GA estimate
+        ga_estimate = genes.dot(halloffame[-1]).round()
+        return ga_estimate
 
 
 if __name__ == '__main__':
@@ -202,19 +225,10 @@ if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(dir_path, '..', 'PythonESN', 'data_backup', 'edgar')
 
-    for hours_elapsed in range(1, 10):  # number of hours in edgar logs
-        # read genes
-        df = pd.read_csv(file_path, nrows=1,
-                         skiprows=hours_elapsed,
-                         header=None,
-                         names=['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS'],  # name the columns
-                         usecols=[0, 1, 2, 3, 4]
-                         )
+    # initialize
+    ga = GeneticAlgorithm(file_path)
 
-        # evolve
-        halloffame = run_ga(hours_elapsed)
-
-        # print best
-        series = df[['Naive', 'AR', 'ARMA', 'ARIMA', 'ETS']].dot(halloffame).round()
-        assert series.size == 1
-        print(series[0])
+    # run for every hour
+    for hr in range(1, 10):
+        result = ga.run(hr)
+        print(result)
