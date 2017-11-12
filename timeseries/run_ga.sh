4b4df1c9bd88a5820601bd630ba01a2cd8e54aa6
Gaurav Kalra <gvkalra@gmail.com>
2017-11-12 10:50:59 +0900

    scriptize GA execution

--- timeseries/run_ga.sh
+++ timeseries/run_ga.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-python -m scoop -n 4 ./evolve.py > "/home/minh/PycharmProjects/Ensemble/timeseries/GA_results"
+########### Variables ###########
+
+# Top most directory of project
+top_dir=$(git rev-parse --show-toplevel)
+
+# Directory path (relative to top_dir) for storing GA results
+results_dir='/timeseries/'
+
+# File name for storing GA results
+results_file='GA_results'
+
+#################################
+
+# Make sure results_dir exists
+dir_path=${top_dir}${results_dir}
+mkdir -p ${dir_path}
+
+# Output path
+result_path=${dir_path}${results_file}
+
+# Execute script
+unbuffer python -m scoop -n 4 evolve.py 2>&1 | tee ${result_path}
