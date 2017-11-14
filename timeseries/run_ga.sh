2d4b3d6f44ceba325c28dc813f75482dc05b7d07
Hoang Minh Nguyen <minhhoang@kaist.ac.kr>
2017-11-14 18:07:09 +0900

    updated scoop to run with python3

---
+++ timeseries/run_ga.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+########### Variables ###########
+DATAFILE="/home/minh/PycharmProjects/Ensemble/PythonESN/data_backup/edgar"
+
+# Top most directory of project
+top_dir=$(git rev-parse --show-toplevel)
+
+# Directory path (relative to top_dir) for storing GA results
+results_dir='/timeseries/'
+
+# File name for storing GA results
+results_file='GA_results'
+
+#################################
+
+# Make sure results_dir exists
+dir_path=${top_dir}${results_dir}
+mkdir -p ${dir_path}
+
+# Output path
+result_path=${dir_path}${results_file}
+
+# Execute script
+unbuffer python3 -m scoop -n 4 evolve.py $DATAFILE 2>&1 | tee ${result_path}
