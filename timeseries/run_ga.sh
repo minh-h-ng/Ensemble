5e000409e3adf72172f6e08fe7d3d431718a3475
Gaurav Kalra <gvkalra@gmail.com>
2017-11-15 22:51:29 +0900

    using rev-parse to find top_dir

--- timeseries/run_ga.sh
+++ timeseries/run_ga.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 ########### Variables ###########
-DATAFILE="/home/minh/PycharmProjects/Ensemble/PythonESN/data_backup/edgar"
+# Top most directory of project
+top_dir=$(git rev-parse --show-toplevel)
+
+# Data
+DATAFILE=${top_dir}"/PythonESN/data_backup/edgar"
 
 # Both hours_start and hours_end are inclusive
 HOURS_START=1866
 HOURS_END=2207
-
-# Top most directory of project
-top_dir=$(git rev-parse --show-toplevel)
 
 # Directory path (relative to top_dir) for storing GA results
 results_dir='/timeseries/'
@@ -27,4 +28,4 @@ result_path=${dir_path}${results_file}
 
 # Execute script
 #unbuffer python3 -m scoop -n 4 evolve.py $DATAFILE $HOURS_ELAPSED 2>&1 | tee ${result_path}
-python3 -m scoop -n 4 evolve.py $DATAFILE $HOURS_START $HOURS_END $result_path
+python3 -m scoop -n 4 evolve.py ${DATAFILE} ${HOURS_START} ${HOURS_END} ${result_path}
