77e2ec5e2dd1b184cb53823a7d7378912b182467
Hoang Minh Nguyen <minhhoang@kaist.ac.kr>
2017-12-24 19:12:03 +0900

    testing some boosting implementations

--- ensemble_algorithms/GBM_draft.py
+++ ensemble_algorithms/GBM_draft.py
@@ -3,6 +3,11 @@ import lightgbm as lgb
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 import warnings
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 cranFile = '/home/minh/PycharmProjects/Ensemble/PythonESN/data_backup/cran_10_12'
 edgarFile = '/home/minh/PycharmProjects/Ensemble/PythonESN/data_backup/edgar_10_12'
@@ -33,20 +38,34 @@ def readDataset(dataFilfe):
     for i in range(len(arList)):
         aList = []
         aList.append(naiveList[i])
-        aList.append(arList[i])
-        aList.append(armaList[i])
-        aList.append(arimaList[i])
-        aList.append(etsList[i])
+        #aList.append(arList[i])
+        #aList.append(armaList[i])
+        #aList.append(arimaList[i])
+        #aList.append(etsList[i])
         X.append(aList)
         y.append(realList[i])
     X_train = X[:-240]
     X_test = X[-240:]
     y_train = y[:-240]
     y_test = y[-240:]
-    return X_train,X_test,y_train,y_test
+
+    Xscaler = StandardScaler()
+    yscaler = MinMaxScaler()
+
+    X_train = Xscaler.fit_transform(X_train)
+    y_train = np.array(y_train).reshape(-1,1)
+    #y_train = y_train.reshape(-1,1)
+    #print('y_train:',y_train)
+    y_train = yscaler.fit_transform(y_train)
+
+    X_test = Xscaler.transform(X_test)
+    y_test = np.array(y_test).reshape(-1, 1)
+    y_test = yscaler.transform(y_test)
+
+    return X_train,X_test,y_train,y_test,Xscaler,yscaler
     #return naiveList,arList,armaList,arimaList,etsList,realList
 
-def GBR(X_train,X_test,y_train,y_test):
+def GBR(X_train,X_test,y_train,y_test,Xscaler,yscaler):
     print('Start training...')
     # train
     """gbm = lgb.LGBMRegressor(objective='regression',
@@ -57,16 +76,13 @@ def GBR(X_train,X_test,y_train,y_test):
     gbm.fit(X_train, y_train,
             eval_set=[(X_test, y_test)],
             eval_metric='l1',
-            early_stopping_rounds=20)
+            #early_stopping_rounds=20
+            )
 
     print('Start predicting...')
     # predict
     y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration_)
-    # eval
-    print('The rmse of prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
-
-    # feature importances
-    print('Feature importances:', list(gbm.feature_importances_))
+    y_pred = yscaler.inverse_transform(np.array(y_pred).reshape(-1,1))
 
     # other scikit-learn modules
     """estimator = lgb.LGBMRegressor(num_leaves=50)
@@ -84,32 +100,53 @@ def GBR(X_train,X_test,y_train,y_test):
 
     #print('y_test:',y_test)
     #print(gbm.predict(X_test))
-    return gbm.predict(X_test)
+    return y_pred
+
+def adaBoost(X_train,X_test,y_train,y_test,Xscaler,yscaler):
+
+    # Fit regression model
+    rng = np.random.RandomState(1)
+    regr_1 = DecisionTreeRegressor(max_depth=40)
+
+    regr_2 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=40),
+                               n_estimators=3000, random_state=rng)
+
+    regr_1.fit(X_train, y_train)
+    regr_2.fit(X_train, y_train)
+
+    # Predict
+    y_1 = regr_1.predict(X_test)
+    y_2 = regr_2.predict(X_test)
+
+    y_1 = yscaler.inverse_transform(np.array(y_1).reshape(-1,1))
+    y_2 = yscaler.inverse_transform(np.array(y_2).reshape(-1,1))
+
+    return y_2
 
 def main():
     warnings.filterwarnings('ignore')
-    X_train,X_test,y_train,y_test = readDataset(cranFile)
-    predictions = GBR(X_train,X_test,y_train,y_test)
+    X_train,X_test,y_train,y_test,Xscaler,yscaler = readDataset(cranFile)
+    predictions = adaBoost(X_train,X_test,y_train,y_test,Xscaler,yscaler)
     with open('/home/minh/PycharmProjects/Ensemble/ensemble_algorithms/results/cran_10_12/Ensemble_GBM_cran_10_12_1','w') as f:
         f.write('Observation,Prediction\n')
         for i in range(len(predictions)):
-            f.write(str(y_test[i]) + ',' + str(predictions[i]))
+            f.write(str(y_test[i][0]) + ',' + str(predictions[i][0]))
             f.write('\n')
 
-    X_train, X_test, y_train, y_test = readDataset(edgarFile)
-    predictions = GBR(X_train, X_test, y_train, y_test)
+    X_train, X_test, y_train, y_test,Xscaler,yscaler = readDataset(edgarFile)
+    predictions = adaBoost(X_train, X_test, y_train, y_test,Xscaler,yscaler)
     with open('/home/minh/PycharmProjects/Ensemble/ensemble_algorithms/results/edgar_10_12/Ensemble_GBM_edgar_10_12_1','w') as f:
         f.write('Observation,Prediction\n')
         for i in range(len(predictions)):
-            f.write(str(y_test[i]) + ',' + str(predictions[i]))
+            f.write(str(y_test[i][0]) + ',' + str(predictions[i][0]))
             f.write('\n')
 
-    X_train, X_test, y_train, y_test = readDataset(kyotoFile)
-    predictions = GBR(X_train, X_test, y_train, y_test)
+    X_train, X_test, y_train, y_test,Xscaler,yscaler = readDataset(kyotoFile)
+    predictions = adaBoost(X_train, X_test, y_train, y_test,Xscaler,yscaler)
     with open('/home/minh/PycharmProjects/Ensemble/ensemble_algorithms/results/kyoto_10_12/Ensemble_GBM_kyoto_10_12_1','w') as f:
         f.write('Observation,Prediction\n')
         for i in range(len(predictions)):
-            f.write(str(y_test[i]) + ',' + str(predictions[i]))
+            f.write(str(y_test[i][0]) + ',' + str(predictions[i][0]))
             f.write('\n')
 
 if __name__ == "__main__":
